0b4f10b4cde69060343e19624441ec686993806d
Andrei Voronov <vorona_80@mail.ru>
2024-06-09 20:43:44 +0300

    first commit

---
+++ src/main/java/com/vorona/java_telegrambot/JavaTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.vorona.java_telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vorona/java_telegrambot/JavaTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vorona.java_telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
